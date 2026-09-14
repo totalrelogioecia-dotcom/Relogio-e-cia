55bb0540045d9120cad6af981b04377f08f2a5a7
totalrelogioecia-dotcom <totalrelogioecia@gmail.com>
2026-09-14 15:34:25 -0300

    Encerra confirmação quando pagamento é aprovado

--- inventory-service.js
+++ inventory-service.js
@@ -1,4 +1,5 @@
 const { shouldApplyPhysicalStock } = require('./product-availability-service');
+const { completeClaimsForOrder } = require('./confirmation-purchase-service');
 
 function physicalRequirements(items) {
   const requirements = new Map();
@@ -41,6 +42,12 @@ function applyPaidOrderStock(products, items) {
 }
 
 function registerStockResult(order, result) {
+  if (order?.id) {
+    completeClaimsForOrder(order.id).catch(error => {
+      console.error('Não foi possível encerrar a confirmação vinculada ao pedido:', error.message);
+    });
+  }
+
   if (result.applied) {
     order.stock_applied = true;
     order.fulfillment_status = 'ready';
