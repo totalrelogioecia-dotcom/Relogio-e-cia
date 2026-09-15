e882865e9b3c421c25e0b85a98861b9bcc4bd1a6
totalrelogioecia-dotcom <totalrelogioecia@gmail.com>
2026-09-14 21:58:00 -0300

    Libera identidade do cabeçalho como recurso público

--- public-static-policy.js
+++ public-static-policy.js
@@ -14,6 +14,7 @@ const PUBLIC_BROWSER_SCRIPTS = new Set([
   'admin-extra-tabs-navigation-fix.js',
   'admin-extra-tabs.js',
   'admin-favorites.js',
+  'admin-header-identity.js',
   'admin-hide-confirm.js',
   'admin-invoice-files.js',
   'admin-melhorenvio.js',
